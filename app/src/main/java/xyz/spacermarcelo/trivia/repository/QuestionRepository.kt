dd927af1d6251837ceadc40bcb4f919a093e0cbe
Marcelo Santos <rocha.mer21@gmail.com>
2022-04-09 17:13:30 -0300

    criado o QuestionRepository

--- app/src/main/java/xyz/spacermarcelo/trivia/repository/QuestionRepository.kt
+++ app/src/main/java/xyz/spacermarcelo/trivia/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package xyz.spacermarcelo.trivia.repository
 
+import android.util.Log
 import xyz.spacermarcelo.trivia.data.DataOrException
 import xyz.spacermarcelo.trivia.model.QuestionItem
 import xyz.spacermarcelo.trivia.network.QuestionApi
@@ -8,10 +9,22 @@ import javax.inject.Inject
 class QuestionRepository @Inject constructor(
     private val api: QuestionApi
 ) {
-    private val listOfQuestions =
+    private val dataOrException =
         DataOrException<ArrayList<QuestionItem>,
                 Boolean,
                 Exception>()
 
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
 
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+
+        } catch (e: Exception) {
+            dataOrException.e = e
+            Log.e("Exc", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+        return dataOrException
+    }
 }
