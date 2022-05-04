42792fb2a454f8b6669fe06bf61433c173333c6f
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-04 13:40:58 -0300

    Creating UI - Radio Button Row

--- app/src/main/java/xyz/spacermarcelo/trivia/component/Questions.kt
+++ app/src/main/java/xyz/spacermarcelo/trivia/component/Questions.kt
@@ -2,14 +2,22 @@ package xyz.spacermarcelo.trivia.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -17,9 +25,9 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import xyz.spacermarcelo.trivia.model.QuestionItem
 import xyz.spacermarcelo.trivia.screens.QuestionsViewModel
 import xyz.spacermarcelo.trivia.util.AppColors
 
@@ -35,9 +43,18 @@ fun Questions(viewModel: QuestionsViewModel) {
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+
     val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
 
     Surface(
@@ -54,6 +71,46 @@ fun QuestionDisplay() {
         ) {
             QuestionTracker()
             DrawDottedLine(pathEffect)
+
+            Column {
+                Text(
+                    text = "Qual é a cor do céu?",
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    color = AppColors.mOffWhite,
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp
+                )
+
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp, brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        AppColors.mOffDarkPurple,
+                                        AppColors.mOffDarkPurple
+                                    )
+                                ), shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50, topEndPercent = 50,
+                                    bottomStartPercent = 50, bottomEndPercent = 50
+                                )
+                            )
+                            .background(Color.Transparent)
+                    ) {
+
+                    }
+                }
+            }
         }
 
     }
--- app/src/main/java/xyz/spacermarcelo/trivia/util/AppColors.kt
+++ app/src/main/java/xyz/spacermarcelo/trivia/util/AppColors.kt
@@ -5,10 +5,11 @@ import androidx.compose.ui.graphics.Color
 object AppColors {
     val mLightGray = Color(0xffb4b9cd)
     val mBlack = Color(0xff060814)
-    val mOffWhite = Color(0xff545a75)
-    val mLightPurple = Color(0xff262C49)
-    val mDarkPurple = Color(0xff22496B)
-    val mLightBlue = Color(0xff127eea)
+    val mOffWhite = Color(0xffeceeef)
+    val mLightPurple = Color(0xff545a75)
+    val mDarkPurple = Color(0xff262C49)
+    val mOffDarkPurple = Color(0xFF22496B)
+    val mLightBlue = Color(0xff127EEA)
     val mBrown = Color(0xff7f6749)
     val mBlue = Color(0xff4b5f9e)
 }
