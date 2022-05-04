c710a972af813374b7d96681cdb5e26ed979dcee
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-04 16:41:21 -0300

    criado o componente - ShowProgress

--- app/src/main/java/xyz/spacermarcelo/trivia/component/Questions.kt
+++ app/src/main/java/xyz/spacermarcelo/trivia/component/Questions.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.ButtonDefaults.buttonColors
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +19,7 @@ import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -96,6 +94,7 @@ fun QuestionDisplay(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
+            if (questionIndex.value >= 3) ShowProgress(score = questionIndex.value)
             viewModel.data.value.data?.let {
                 QuestionTracker(
                     counter = questionIndex.value,
@@ -249,6 +248,10 @@ fun ShowProgress(score: Int = 12) {
         )
     )
 
+    val progressFactor by remember(score) {
+        mutableStateOf(score * 0.005f)
+    }
+
     Row(
         modifier = Modifier
             .padding(3.dp)
@@ -278,7 +281,7 @@ fun ShowProgress(score: Int = 12) {
             contentPadding = PaddingValues(1.dp),
             onClick = {},
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxWidth(progressFactor)
                 .background(brush = gradient),
             enabled = false,
             elevation = null,
@@ -287,7 +290,16 @@ fun ShowProgress(score: Int = 12) {
                 disabledBackgroundColor = Color.Transparent
             )
         ) {
-
+            Text(
+                text = (score * 10).toString(),
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(23.dp))
+                    .fillMaxHeight(0.87f)
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                color = AppColors.mOffWhite,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
