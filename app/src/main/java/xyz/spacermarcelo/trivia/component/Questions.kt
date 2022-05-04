dfe921ea7dbccf829051698dece241f84720f6fb
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-04 16:13:07 -0300

    Finishing up the Logic - App Working

--- app/src/main/java/xyz/spacermarcelo/trivia/component/Questions.kt
+++ app/src/main/java/xyz/spacermarcelo/trivia/component/Questions.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -31,11 +32,24 @@ import xyz.spacermarcelo.trivia.util.AppColors
 @Composable
 fun Questions(viewModel: QuestionsViewModel) {
     val questions = viewModel.data.value.data?.toMutableList()
+
+    val questionIndex = remember {
+        mutableStateOf(0)
+    }
+
     if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
     } else {
+        val question = try {
+            questions?.get(questionIndex.value)
+        } catch (ex: Exception) {
+            null
+        }
+
         if (questions != null) {
-            QuestionDisplay(questions.first())
+            QuestionDisplay(question!!, questionIndex, viewModel) {
+                questionIndex.value += 1
+            }
 
         }
     }
@@ -45,8 +59,8 @@ fun Questions(viewModel: QuestionsViewModel) {
 @Composable
 fun QuestionDisplay(
     question: QuestionItem,
-    //questionIndex: MutableState<Int>,
-    //viewModel: QuestionsViewModel,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
     onNextClicked: (Int) -> Unit = {}
 ) {
     val choicesState = remember(question) {
@@ -73,8 +87,7 @@ fun QuestionDisplay(
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(4.dp),
+            .fillMaxHeight(),
         color = AppColors.mDarkPurple
     ) {
         Column(
@@ -82,7 +95,12 @@ fun QuestionDisplay(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
-            QuestionTracker()
+            viewModel.data.value.data?.let {
+                QuestionTracker(
+                    counter = questionIndex.value,
+                    outOf = it.size
+                )
+            }
             DrawDottedLine(pathEffect)
 
             Column {
@@ -134,8 +152,42 @@ fun QuestionDisplay(
                                 } else Color.Red.copy(alpha = 0.2f)
                             )
                         )
-                        Text(answerText)
+                        val annotatedString = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.Light,
+                                    color = if (correctAnswerState.value == true && index == answerState.value) {
+                                        Color.Green
+                                    } else if (correctAnswerState.value == false && index == answerState.value) {
+                                        Color.Red
+                                    } else AppColors.mOffWhite,
+                                    fontSize = 17.sp
+                                )
+                            ) {
+                                append(answerText)
+                            }
+                        }
+
+                        Text(text = annotatedString, modifier = Modifier.padding(6.dp))
                     }
+                }
+
+                Button(
+                    onClick = { onNextClicked(questionIndex.value) },
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(34.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = AppColors.mLightBlue
+                    )
+                ) {
+                    Text(
+                        text = "Next",
+                        modifier = Modifier.padding(4.dp),
+                        color = AppColors.mOffWhite,
+                        fontSize = 17.sp
+                    )
                 }
             }
         }
